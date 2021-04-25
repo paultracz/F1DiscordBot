9f7a92d9920219350cd5926402cbd87fcaaad6b1
PolzPT <paultracz07@gmail.com>
2021-04-25 00:34:03 -0400

    nextRace Changes Modified date and time format Added time remaining to nextRace

--- commands/nextRace.js
+++ commands/nextRace.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed } = require('discord.js')
 
 module.exports = {
     slash: 'both',
@@ -7,60 +7,71 @@ module.exports = {
     description: "Displays information for the upcoming F1 race",
     callback: ({ message }) => {
         const embed = new MessageEmbed()
-        .setTitle('Upcoming Race')
-        
+            .setTitle('Upcoming Race')
+
         async function getData() {
             try {
-                const response = await axios.get('http://ergast.com/api/f1/current/next.json?limit=1');
-                const responseTotal = await axios.get('http://ergast.com/api/f1/current.json');
-                var totalRounds = (responseTotal.data.MRData.total);
-                var round = (response.data.MRData.RaceTable.round);
-                var raceName = (response.data.MRData.RaceTable.Races[0].raceName);
-                var date = (response.data.MRData.RaceTable.Races[0].date);
-                var time = (response.data.MRData.RaceTable.Races[0].time);
-                var parsedDate = date + 'T' + time;
-                var newDate = new Date(parsedDate);
-                var weekday = getDayOfWeek(newDate);
-                var month = getMonthOfYear(newDate);
-                var dayOfMonth = newDate.getDate();
-                var year = new Date(date).getFullYear();
-                var localTime = newDate.toLocaleTimeString();
-                var output = ('\nRound: ' + round
-                    + '/' + totalRounds
-                    + '\nRace: ' + raceName
-                    + '\nDate: ' + weekday
-                    + ', ' + month
-                    + ' ' + dayOfMonth
-                    + ', ' + year
-                    + '\nTime: ' + localTime);
+                const response = await axios.get('http://ergast.com/api/f1/current/next.json?limit=1')
+                const responseTotal = await axios.get('http://ergast.com/api/f1/current.json')
+                var totalRounds = (responseTotal.data.MRData.total)
+                var round = (response.data.MRData.RaceTable.round)
+                var raceName = (response.data.MRData.RaceTable.Races[0].raceName)
+                var circuitName = (response.data.MRData.RaceTable.Races[0].Circuit.circuitName)
+                var date = (response.data.MRData.RaceTable.Races[0].date)
+                var time = (response.data.MRData.RaceTable.Races[0].time)
+                var parsedDate = date + 'T' + time
+                var newDate = new Date(parsedDate)
+                var weekday = getDayOfWeek(newDate)
+                var month = getMonthOfYear(newDate)
+                var dayOfMonth = newDate.getDate()
+                var year = new Date(date).getFullYear()
+                var localTime = newDate.toLocaleTimeString()
+                var currentDate = new Date()
+                const diffTime = Math.abs(newDate - currentDate)
+                const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24))
+                var diffHours, diffMins
+                if (diffDays >= 1) {
+                    diffHours = Math.floor((diffTime - diffDays * 1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+                    diffMins = Math.ceil((diffTime - (diffHours * 1000 * 60 *60) - (diffDays * 1000 * 60 * 60 * 24)) / (1000 * 60));
+                }
+                else {
+                    diffHours = Math.floor(diffTime / (1000 * 60 * 60));
+                    diffMins = Math.ceil((diffTime - (diffHours * 1000 * 60 * 60)) / (1000 * 60));
+                }                           
 
                 embed.addField('Round', (round + '/' + totalRounds))
                 embed.addField('Race', raceName)
-                embed.addField('Date', weekday + ', ' + month
-                    + ' ' + dayOfMonth + ', ' + year)
-                embed.addField('Time', localTime)
+                embed.addField('Circuit', circuitName)
+                embed.addField('Date and Time', weekday + ', ' + month
+                    + ' ' + dayOfMonth + ', ' + year + '\n'
+                    + localTime.substr(0, 5) + localTime.substr(-5))
+                embed.addField('Time Remaining', diffDays + " days, "
+                    + diffHours + " hours, " +
+                    + diffMins + " mins")
+            
+
                 if (message) {
-                    message.reply('', {embed});
+                    message.reply('', { embed })
                 }
 
             } catch (error) {
-                console.error(error);
+                console.error(error)
             }
-            return embed;
+            return embed
         }
-        return getData();
+        return getData()
     }
 }
 
 function getDayOfWeek(date) {
-    const dayOfWeek = new Date(date).getDay();
+    const dayOfWeek = new Date(date).getDay()
     return isNaN(dayOfWeek) ? null :
-        ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][dayOfWeek];
+        ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][dayOfWeek]
 }
 
 function getMonthOfYear(date) {
-    const monthOfYear = new Date(date).getMonth();
+    const monthOfYear = new Date(date).getMonth()
     return isNaN(monthOfYear) ? null :
         ['January', 'February', 'March', 'April', 'May', 'June', 'July'
-            , 'August', 'September', 'October', 'November', 'December'][monthOfYear];
+            , 'August', 'September', 'October', 'November', 'December'][monthOfYear]
 }
