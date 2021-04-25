3bfdc689c8c51848ae1236ca3919c8f028c7d83f
PolzPT <paultracz07@gmail.com>
2021-04-25 17:18:26 -0400

    for loop changes

--- commands/standings.js
+++ commands/standings.js
@@ -10,20 +10,20 @@ module.exports = {
     callback: async ({ message, args }) => {
         const embed = new MessageEmbed()
         async function getData() {
-        const [input] = args;
+            const [input] = args;
             if (input.toLowerCase() === 'drivers') {
                 embed.setTitle('Driver Standings');
                 const response = await axios.get('http://ergast.com/api/f1/current/driverstandings.json?limit=20');
-                const data = response.data.MRData.StandingsTable.StandingsLists[0];                
+                const data = response.data.MRData.StandingsTable.StandingsLists[0];
                 var positions = [];
                 var drivers = [];
                 var points = [];
-                for (var i = 0; i < data.DriverStandings.length; i++) {
-                    var obj = data.DriverStandings[i];
+                data.DriverStandings.forEach(obj => {
                     var fullName = obj.Driver.givenName + ' ' + obj.Driver.familyName;
                     positions.push(obj.position);
                     drivers.push(fullName);
                     points.push(obj.points);
+                }); {
                 }
                 embed.addField('Pos', positions, inline = true)
                 embed.addField('Driver', drivers, inline = true)
@@ -36,12 +36,11 @@ module.exports = {
                 var positions = [];
                 var teams = [];
                 var points = [];
-                for (var i = 0; i < data.ConstructorStandings.length; i++) {
-                    var obj = data.ConstructorStandings[i];
+                data.ConstructorStandings.forEach(obj => {
                     positions.push(obj.position);
                     teams.push(obj.Constructor.name);
                     points.push(obj.points);
-                }
+                });
                 embed.addField('Pos', positions, inline = true)
                 embed.addField('Driver', teams, inline = true)
                 embed.addField('Points', points, inline = true)
